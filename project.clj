ad971feb6c3c3a674c1207eb8911ed6f8699bc93
Alex Redington <alex.redington@thinkrelevance.com>
2013-05-17 13:12:15 -0400

    Fix copy-paste error pointed out by @bronsa

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject core.async "0.1.0-SNAPSHOT"
   :description "Facilities for async programming and communication in Clojure"
-  :url "https://github.com/clojure/core.rrb-vector"
+  :url "https://github.com/clojure/core.async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :parent [org.clojure/pom.contrib "0.1.2"]
