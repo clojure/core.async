b80258f8b03ad490c4401a16ba33741ce9c7b934
Alex Redington <alex.redington@thinkrelevance.com>
2013-05-17 12:20:32 -0400

    WIP Timers
    - Timers implemented
    - Commented tests require further fleshing out in channels before they can pass
    - Re-enable tests in timers_test when channels are done.

--- src/clj/core/async/timers.clj
+++ src/clj/core/async/timers.clj
@@ -7,11 +7,46 @@
 ;;   You must not remove this notice, or any other, from this software.
 
 (ns core.async.timers
-  (:require [core.async.protocols :as proto])
-  (:import [java.util.concurrent DelayQueue]))
+  (:require [core.async.protocols :as proto]
+            [core.async.channels :as channels])
+  (:import [java.util.concurrent DelayQueue Delayed TimeUnit]
+           [clojure.lang IDeref]))
 
 (set! *warn-on-reflection* true)
 
+(defonce ^:private timeouts-queue
+  (DelayQueue.))
+
+(deftype TimeoutQueueEntry [channel timestamp]
+  Delayed
+  (getDelay [this time-unit]
+    (.convert time-unit
+              (- timestamp (System/currentTimeMillis))
+              TimeUnit/MILLISECONDS))
+  (compareTo [this other]
+    (.compareTo ^Long (.getDelay this TimeUnit/MILLISECONDS)
+                ^Long (.getDelay ^Delayed other TimeUnit/MILLISECONDS)))
+  IDeref
+  (deref [this]
+    channel))
+
 (defn timeout
   "returns a channel that will close after msecs"
-  [msecs])
+  [msecs]
+  (let [timeout (+ (System/currentTimeMillis) msecs)
+        timeout-channel (channels/chan nil)
+        timeout-delay (TimeoutQueueEntry. timeout-channel timeout)]
+    (locking timeouts-queue
+      (.put ^DelayQueue timeouts-queue timeout-delay))
+    timeout-channel))
+
+(defn timeout-channel
+  []
+  (let [timeout-delay (.take ^DelayQueue timeouts-queue)]
+    (proto/close! @timeout-delay))
+  (recur))
+
+(defonce timeout-daemon
+  (doto (Thread. ^Runnable timeout-channel "core.async.timers/timeout-daemon")
+    (.setDaemon true)
+    (.start)))
---
+++ test/core/async/timers_test.clj
@@ -0,0 +1,28 @@
+(ns core.async.timers-test
+  (:require [clojure.test :refer :all]
+            [core.async.timers :refer :all]
+            [core.async :as async]))
+
+(deftest timeout-interval-test
+  #_(let [start-stamp (System/currentTimeMillis)
+        test-timeout (timeout 500)]
+    (is (>= (+ start-stamp 500)
+            (do (async/<! test-timeout)
+                (System/currentTimeMillis)))
+        "Reading from a timeout channel does not complete until the specified milliseconds have elapsed.")))
+
+(deftest timeout-ordering-test
+  #_(let [test-atom (atom [])
+        timeout-channels [(timeout 800)
+                          (timeout 600)
+                          (timeout 700)
+                          (timeout 500)]
+        insert-vals [:d :b :c :a]
+        threads (dorun (for [i (range 4)]
+                         (doto (Thread. #(do (async/<! (timeout-channels i))
+                                             (swap! test-atom conj (insert-vals i))))
+                           (.start))))]
+    (doseq [thread threads]
+      (.join thread))
+    (is (= @test-atom [:a :b :c :d])
+        "Timeouts close in order determined by their delays, not in order determined by their creation.")))
