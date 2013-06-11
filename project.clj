4dae5d69d5d7dbae11912186f07ac74c747e3692
Timothy Baldridge <timothy@thinkrelevance.com>
2013-06-10 22:29:50 -0600

    forgot to take out criterium before commit

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :parent [org.clojure/pom.contrib "0.1.2"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-		 [criterium "0.4.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :warn-on-reflection true
   :source-paths ["src/clj"]
   :jvm-opts ["-Xmx1g" "-server"]
