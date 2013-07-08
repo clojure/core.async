ef8f2c2ed4d03abf0e01c3e9550b75bb4cd0f8d5
Alex Miller <alex.miller@thinkrelevance.com>
2013-07-08 06:44:26 -0500

    remove deprecated lein-ism for checking reflection warnings

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :parent [org.clojure/pom.contrib "0.1.2"]
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :warn-on-reflection true
+  :global-vars {*warn-on-reflection* true}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
