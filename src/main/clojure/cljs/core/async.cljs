6eb688ebb9d868e188902af7fb245598467fb892
David Nolen <david.nolen@gmail.com>
2013-07-16 08:33:38 -0400

    add missing cljs async require for dispatch ns

--- src/main/clojure/cljs/core/async.cljs
+++ src/main/clojure/cljs/core/async.cljs
@@ -3,6 +3,7 @@
               [cljs.core.async.impl.channels :as channels]
               [cljs.core.async.impl.buffers :as buffers]
               [cljs.core.async.impl.timers :as timers]
+              [cljs.core.async.impl.dispatch :as dispatch]
               [cljs.core.async.impl.ioc-helpers :as helpers]))
 
 
