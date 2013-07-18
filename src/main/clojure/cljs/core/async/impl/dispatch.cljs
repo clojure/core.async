62bf0ea51113c32182d8e90f38de85e558dde3f4
David Nolen <david.nolen@gmail.com>
2013-07-18 12:38:34 -0400

    use MessageChannel when available

--- src/main/clojure/cljs/core/async/impl/dispatch.cljs
+++ src/main/clojure/cljs/core/async/impl/dispatch.cljs
@@ -1,7 +1,23 @@
 (ns cljs.core.async.impl.dispatch)
 
+(def message-channel nil)
+(def tasks nil)
+
+(when (.-MessageChannel js/window)
+  (set! message-channel (js/MessageChannel.))
+  (set! tasks (array))
+  (set! (.. message-channel -port1 -onmessage)
+    (fn [msg]
+      ((.shift tasks)))))
+
+(defn queue-task [f]
+  (.push tasks f)
+  (.postMessage (.-port2 message-channel) 0))
+
 (defn run [f]
-  (js/setTimeout f 0))
+  (if-not (nil? message-channel)
+    (queue-task f)
+    (js/setTimeout f 0)))
 
 (defn queue-delay [f delay]
   (js/setTimeout f delay))
