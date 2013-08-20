e2f6a689a416a21ada71affaa98ea4de9d1bb86a
David Nolen <david.nolen@gmail.com>
2013-08-20 12:58:36 -0400

    experiment with tick, run task immediately unless tick > 1024, host dispatch and reset

--- src/main/clojure/cljs/core/async/impl/dispatch.cljs
+++ src/main/clojure/cljs/core/async/impl/dispatch.cljs
@@ -2,6 +2,8 @@
 
 (def message-channel nil)
 (def tasks nil)
+(def MAX_TICK 1024)
+(def tick 0)
 
 (when (exists? js/MessageChannel)
   (set! message-channel (js/MessageChannel.))
@@ -15,10 +17,14 @@
   (.postMessage (.-port2 message-channel) 0))
 
 (defn run [f]
-  (cond
-    (exists? js/MessageChannel) (queue-task f)
-    (exists? js/setImmediate) (js/setImmediate f)
-    :else (js/setTimeout f 0)))
+  (if (< tick MAX_TICK)
+    (do (set! tick (inc tick))
+      (f))
+    (do (set! tick 0)
+      (cond
+        (exists? js/MessageChannel) (queue-task f)
+        (exists? js/setImmediate) (js/setImmediate f)
+        :else (js/setTimeout f 0)))))
 
 (defn queue-delay [f delay]
   (js/setTimeout f delay))
