44b1e30e11984af3fe6b1b5796631e19ddd44338
Timothy Baldridge <tbaldridge@gmail.com>
2013-09-27 11:26:57 -0600

    switch to latest ClojureScript, enable source maps in adv mode for tests

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :parent [org.clojure/pom.contrib "0.1.2"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1878"]]
+                 [org.clojure/clojurescript "0.0-1909"]]
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
@@ -26,5 +26,8 @@
     {:id "adv"
      :source-paths ["src/test/cljs" "src/main/clojure/cljs"]
      :compiler {:optimizations :advanced
-                :pretty-print true
-                :output-to "tests.js"}}]})
+                :pretty-print false
+                :static-fns true
+                :output-dir "out"
+                :output-to "tests.js"
+                :source-map "tests.js.map"}}]})
