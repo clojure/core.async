clojure/core.async

commit 3c202d484029da109bc3099f4570998c442bd866

support early termination via reduced in cljs

src/main/clojure/cljs/core/async/impl/channels.cljs
@@ -24,7 +24,23 @@
 
 (def ^:const MAX_DIRTY 64)
 
+(defprotocol MMC
+  (abort [this]))
+
 (deftype ManyToManyChannel [takes ^:mutable dirty-takes puts ^:mutable dirty-puts ^not-native buf ^:mutable closed add!]
+  MMC
+  (abort [this]
+    (loop []
+      (let [putter (.pop puts)]
+        (when-not (nil? putter)
+          (let [^not-native put-handler (.-handler putter)
+                val (.-val putter)]
+            (if ^boolean (impl/active? put-handler)
+              (let [put-cb (impl/commit put-handler)]
+                (dispatch/run #(put-cb true)))
+              (recur))))))
+    (.cleanup puts (constantly false))
+    (impl/close! this))
   impl/WritePort
   (put! [this val ^not-native handler]
     (assert (not (nil? val)) "Can't put nil in on a channel")
@@ -35,17 +51,17 @@
         (if (and buf (not (impl/full? buf)))
           (do
             (impl/commit handler)
-            (add! buf val)
-            (loop []
-              (let [^not-native taker (.pop takes)]
-                (if-not (nil? taker)
-                  (if ^boolean (impl/active? taker)
-                    (let [take-cb (impl/commit taker)
-                          val (impl/remove! buf)]
-                      (dispatch/run (fn [] (take-cb val)))
-                      (box true))
-                    (recur))
-                  (box true)))))
+            (let [done? (reduced? (add! buf val))]
+              (loop []
+                (let [^not-native taker (.pop takes)]
+                  (if-not (nil? taker)
+                    (if ^boolean (impl/active? taker)
+                      (let [take-cb (impl/commit taker)
+                            val (impl/remove! buf)]
+                        (dispatch/run (fn [] (take-cb val))))
+                      (recur)))))
+              (when done? (abort this))
+              (box true)))
           (do
             (if (> dirty-puts MAX_DIRTY)
               (do (set! dirty-puts 0)
@@ -73,7 +89,8 @@
                       (let [put-cb (impl/commit put-handler)
                             _ (impl/commit handler)]
                         (dispatch/run #(put-cb true))
-                        (add! buf val))
+                      (when (reduced (add! buf val))
+                        (abort this)))
                       (recur))))))
           retval)
         (loop []

commit 76142bff323023c8359c917c7544f61229e2a18d

Fix regression introduced in 3c202d48
Early termination was *always* happening.

Also added test to detect this case.

src/main/clojure/cljs/core/async/impl/channels.cljs
@@ -89,7 +89,7 @@
                       (let [put-cb (impl/commit put-handler)
                             _ (impl/commit handler)]
                         (dispatch/run #(put-cb true))
-                      (when (reduced (add! buf val))
+                      (when (reduced? (add! buf val))
                         (abort this)))
                       (recur))))))
           retval)

src/test/cljs/cljs/core/async/tests.cljs
@@ -268,3 +268,20 @@
                    (<! (timeout 500))
                    ;; Thus this should be 2
                    (is= @a 2)))))))
+(defn integer-chan
+  "Returns a channel upon which will be placed integers from 0 to n (exclusive) at 10 ms intervals, using the provided xform"
+  [n xform]
+  (let [c (chan 1 xform)]
+    (go
+      (loop [i 0]
+        (if (< i n)
+          (do
+            (<! (timeout 10))
+            (>! c i)
+            (recur (inc i)))
+          (close! c))))
+    c))
+
+(deftest test-transducers
+         (testing "base case without transducer"
+                  (go (is (= (range 10) (<! (async/into [] (integer-chan 10 nil))))))))