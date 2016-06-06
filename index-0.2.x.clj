4473fedee28508ff1a341fc53b5f5b8ef62b397c
Tom Faulhaber <git_com@infolace.com>
2016-06-06 12:32:47 -0700

    Autodoc commit for master/07e70e47

--- index-0.2.x.clj
+++ index-0.2.x.clj
@@ -1,15 +1,14 @@
 {:namespaces
  ({:doc nil,
    :name "clojure.core.async",
-   :wiki-url
-   "http://clojure.github.io/core.async/clojure.core.async-api.html",
+   :wiki-url "http://clojure.github.io/core.async/index.html",
    :source-url
    "https://github.com/clojure/core.async/blob/a6dc01d2ebb692a39e923988b0853ff2bd1a431b/src/main/clojure/clojure/core/async.clj"}
   {:doc
    "core.async HIGHLY EXPERIMENTAL feature exploration\n\nCaveats:\n\n1. Everything defined in this namespace is experimental, and subject\nto change or deletion without warning.\n\n2. Many features provided by this namespace are highly coupled to\nimplementation details of core.async. Potential features which\noperate at higher levels of abstraction are suitable for inclusion\nin the examples.\n\n3. Features provided by this namespace MAY be promoted to\nclojure.core.async at a later point in time, but there is no\nguarantee any of them will.",
    :name "clojure.core.async.lab",
    :wiki-url
-   "http://clojure.github.io/core.async/clojure.core.async-api.html#clojure.core.async.lab",
+   "http://clojure.github.io/core.async/index.html#clojure.core.async.lab",
    :source-url
    "https://github.com/clojure/core.async/blob/ad7ca68a831a1e469b6f30334903279b2e4e43f7/src/main/clojure/clojure/core/async/lab.clj"}),
  :vars
