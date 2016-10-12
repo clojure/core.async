5e2b15687ef377eb45ebcc491a73f7e3168b1106
puredanger <alex.miller@cognitect.com>
2016-10-12 16:59:54 -0500

    add transduce test

--- src/test/clojure/clojure/core/async_test.clj
+++ src/test/clojure/clojure/core/async_test.clj
@@ -368,3 +368,18 @@
   (doseq [b (range 1 10)
           t (range 1 10)]
     (check-expanding-transducer b 3 3 t)))
+
+;; in 1.7+, use (map f)
+(defn mapping [f]
+  (fn [f1]
+    (fn
+      ([] (f1))
+      ([result] (f1 result))
+      ([result input]
+       (f1 result (f input)))
+      ([result input & inputs]
+       (f1 result (apply f input inputs))))))
+
+(deftest test-transduce
+  (is (= [1 2 3 4 5]
+         (<!! (a/transduce (mapping inc) conj [] (a/to-chan (range 5)))))))
