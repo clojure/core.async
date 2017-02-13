73afa795a512f2113f715b4c739bf3db479c8a08
puredanger <alex.miller@cognitect.com>
2017-02-13 15:27:13 -0600

    ASYNC-70 - Mention that returned channel is closed for thread and thread-call

--- src/main/clojure/clojure/core/async.clj
+++ src/main/clojure/clojure/core/async.clj
@@ -428,7 +428,7 @@ the Java system property `clojure.core.async.pool-size`."
 (defn thread-call
   "Executes f in another thread, returning immediately to the calling
   thread. Returns a channel which will receive the result of calling
-  f when completed."
+  f when completed, then close."
   [f]
   (let [c (chan 1)]
     (let [binds (clojure.lang.Var/getThreadBindingFrame)]
@@ -446,7 +446,7 @@ the Java system property `clojure.core.async.pool-size`."
 (defmacro thread
   "Executes the body in another thread, returning immediately to the
   calling thread. Returns a channel which will receive the result of
-  the body when completed."
+  the body when completed, then close."
   [& body]
   `(thread-call (fn [] ~@body)))
 
