cbf8778bd7b24a3102056d21eab1f1cc2860dd95
Mike Fikes <mike@fikesfarm.com>
2017-07-15 08:58:20 -0400

    ASYNC-201: (CLJS) Out-of-bounds index values passed in timers

--- src/test/cljs/cljs/core/async/test_runner.cljs
+++ src/test/cljs/cljs/core/async/test_runner.cljs
@@ -2,9 +2,11 @@
   (:require [cljs.test :refer-macros [run-tests]]
             [cljs.core.async.buffer-tests]
             [cljs.core.async.pipeline-test]
+            [cljs.core.async.timers-test]
             [cljs.core.async.tests]))
 
 (run-tests
   'cljs.core.async.pipeline-test
   'cljs.core.async.buffer-tests
+  'cljs.core.async.timers-test
   'cljs.core.async.tests)
