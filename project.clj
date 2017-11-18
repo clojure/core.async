3d1adb31371e54641bbedf7d882bc193a0e0b5e8
Mike Fikes <mike@fikesfarm.com>
2017-11-18 08:57:38 -0500

    0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject andare "0.7.0"
+(defproject andare "0.8.0"
   :description "core.async for bootstrap ClojureScript"
   :url "https://github.com/mfikes/andare"
   :license {:name "Eclipse Public License"
