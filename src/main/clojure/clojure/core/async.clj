3429e3e1f1d49403bf9608b36dbd6715ffe4dd4f
Alex Miller <alex.miller@cognitect.com>
2019-10-30 09:08:10 -0500

    Use explicit threads for pipeline, not go blocks

--- src/main/clojure/clojure/core/async.clj
+++ src/main/clojure/clojure/core/async.clj
@@ -503,14 +503,10 @@ the Java system property `clojure.core.async.pool-size`."
                        true)))]
        (dotimes [_ n]
          (case type
-               :blocking (thread
-                          (let [job (<!! jobs)]
-                            (when (process job)
-                              (recur))))
-               :compute (go-loop []
-                                   (let [job (<! jobs)]
-                                     (when (process job)
-                                       (recur))))
+               (:blocking :compute) (thread
+                                      (let [job (<!! jobs)]
+                                        (when (process job)
+                                          (recur))))
                :async (go-loop []
                                  (let [job (<! jobs)]
                                    (when (async job)
