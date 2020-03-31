e2fcb458b58f8aed2dd0390903338c9331bf0620
dnolen <david.nolen@gmail.com>
2020-03-31 15:24:34 -0400

    add missing copyright comments, add EXPERIMENTAL to interop docstrings

--- src/main/clojure/cljs/core/async/interop.cljs
+++ src/main/clojure/cljs/core/async/interop.cljs
@@ -1,8 +1,16 @@
+;;   Copyright (c) Rich Hickey and contributors. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 (ns cljs.core.async.interop
   (:require [cljs.core.async :as async]))
 
 (defn p->c
-  "Puts the promise resolution into a promise-chan and returns it.
+  "EXPERIMENTAL: Puts the promise resolution into a promise-chan and returns it.
    The value of a rejected promise will be wrapped in a instance of
    ExceptionInfo, acessible via ex-cause."
   [p]
--- src/test/cljs/cljs/core/async/runner_tests.cljs
+++ src/test/cljs/cljs/core/async/runner_tests.cljs
@@ -1,3 +1,11 @@
+;;   Copyright (c) Rich Hickey and contributors. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 (ns cljs.core.async.runner-tests
   (:require [cljs.core.async :refer [buffer dropping-buffer sliding-buffer put! take! chan close!]]
             [cljs.core.async.impl.dispatch :as dispatch]
